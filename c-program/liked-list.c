4de5b04d708c23750aa0994f5db2864f63587ade
Harshith Gowda <harshithgowdakt@gmail.com>
2020-11-22 20:38:05 +0530

    linked list

---
+++ c-program/liked-list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+  int data;
+  struct Node *next;
+};
+
+int main()
+{
+    struct Node *head = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *second = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *third = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 10;
+    head->next = second;
+    second->data = 20;
+    second->next = third;
+    third->data = 30;
+    third->next = NULL;
+}
