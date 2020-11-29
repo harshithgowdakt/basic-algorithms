5b0e2cc5bb041a947ca0990444dc4f633e4e699a
Harshith Gowda <harshithgowdakt@gmail.com>
2020-11-29 19:48:37 +0530

    intersion to list

--- c-program/liked-list.c
+++ c-program/liked-list.c
@@ -16,6 +16,44 @@ void print_list(struct Node *n)
   }
 }
 
+void insert_at_beginning(struct Node **head, int data)
+{
+  struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+  new_node->data = data;
+  new_node->next = *head;
+  *head = new_node;
+}
+
+void insert_at_end(struct Node *head, int data)
+{
+  struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+  new_node->data = data;
+  new_node->next = NULL;
+  struct Node *temp_node = head;
+  while (temp_node->next != NULL)
+  {
+    temp_node = temp_node->next;
+  }
+  temp_node->next = new_node;
+}
+
+void insert_at_position(struct Node *head, int position, int data)
+{
+  struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+  new_node->data = data;
+  new_node->next = NULL;
+  struct Node *temp_node = head;
+  for (int i = 2; i <= position; i++)
+  {
+    if (temp_node->next != NULL)
+    {
+      temp_node = temp_node->next;
+    }
+  }
+  new_node->next = temp_node->next;
+  temp_node->next = new_node;
+}
+
 int main()
 {
     struct Node *head = (struct Node *)malloc(sizeof(struct Node));
@@ -28,5 +66,8 @@ int main()
     second->next = third;
     third->data = 30;
     third->next = NULL;
+    insert_at_beginning(&head, 40);
+    insert_at_end(head, 50);
+    insert_after_position(head, 1, 60);
     print_list(head);
 }
