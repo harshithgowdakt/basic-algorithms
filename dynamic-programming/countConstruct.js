36bcb1384b3618d141d0a36812fa88a1b2b3e932
Harshith Gowda <harshithgowdakt@gmail.com>
2021-10-11 00:17:25 +0530

    count construct

---
+++ dynamic-programming/countConstruct.js
@@ -0,0 +1,21 @@
+// check hom many wasys we can construct target string using word bank
+// words can be reused
+
+function countConstruct(targetString, wordBank, memo = {}) {
+    if (targetString in memo) return targetString[memo];
+    if (targetString.length === 0) return 1;
+
+    let count = 0;
+    for (let str of wordBank) {
+        if (targetString.indexOf(str) === 0) {
+            let subString = targetString.slice(str.length);
+            let result = countConstruct(subString, wordBank, memo);
+            count += result;
+        }
+    }
+    return count;
+}
+console.log(countConstruct('abcd', ['a', 'b', 'c', 'd', 'abcd', 'ab', 'cd', 'abc', 'bcd']));
+console.log(countConstruct('abcdef', ['ab', 'abctrue', 'cd', 'def', 'abcd']));
+console.log(countConstruct('abcdedewq', ['awd', 'qwdqw', 'qwdw', 'asd', 'add']));
+
