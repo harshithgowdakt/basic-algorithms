c3ae6338af0b784071c5b7f2084073e2ce5d8781
Harshith Gowda <harshithgowdakt@gmail.com>
2021-10-10 20:47:41 +0530

    best sum

--- dynamic-programming/bestSum.js
+++ dynamic-programming/bestSum.js
@@ -1,15 +1,15 @@
 // is is possible to generate targetSum using the numbers in an array
 // numbers will be allways positive and 
-// exmpale 7, [2,3,4,5, 7] should return [3,4] or [7] as we can generate 7 using 3 and 4 or 7;
+// exmpale 7, [2,3,4,5, 7] should return [7] as we can generate 7 using  7;
 
-function howSum(targetSum, numbers, memo = {}) {
+function bestSum(targetSum, numbers, memo = {}) {
     if (targetSum in memo) return memo[targetSum];
     if (targetSum === 0) return [];
     if (targetSum < 0) return null;
 
     for (let num of numbers) {
         let remainder = targetSum - num;
-        let result = howSum(remainder, numbers, memo);
+        let result = bestSum(remainder, numbers, memo);
         if (result !== null) {
             memo[targetSum] = [...result, num];
             return memo[targetSum];
@@ -19,8 +19,8 @@ function howSum(targetSum, numbers, memo = {}) {
     return null;
 }
 
-console.log(howSum(7, [2, 3]));
-console.log(howSum(7, [5, 3, 4, 7]));
-console.log(howSum(7, [2, 1, 3, 4, 5, 6, 8]));
-console.log(howSum(8, [2, 3, 5]));
-console.log(howSum(300, [7, 14]));
+console.log(bestSum(7, [2, 3].sort((a, b) => b - a)));
+console.log(bestSum(7, [5, 3, 4, 7].sort((a, b) => b - a)));
+console.log(bestSum(7, [2, 1, 3, 4, 5, 6, 8].sort((a, b) => b - a)));
+console.log(bestSum(8, [2, 3, 5].sort((a, b) => b - a)));
+console.log(bestSum(300, [7, 14].sort((a, b) => b - a)));
