a643b1243a573e45c3d97abef9907e9fa6fa9f8a
Harshith Gowda <harshithgowdakt@gmail.com>
2021-12-08 23:37:54 +0530

    Wed  8 Dec 23:37:54 IST 2021 : middle element

--- linked-list/middleElement.js
+++ linked-list/middleElement.js
@@ -6,10 +6,22 @@ function Node(data) {
     this.next = null;
 }
 
+function middleElement(head) {
+    let slowPointer = head, fastPointer = head;
 
+    while (fastPointer !== null && fastPointer.next !== null) {
+        fastPointer = fastPointer.next.next;
+        slowPointer = slowPointer.next;
+    }
+    return slowPointer.data;
+}
 
 function main() {
     let head = new Node(1);
     head.next = new Node(2);
     head.next.next = new Node(3);
+    head.next.next.next = new Node(4);
+    head.next.next.next.next = new Node(5);
+    console.log("middle element of list is", middleElement(head));
 }
+main()
