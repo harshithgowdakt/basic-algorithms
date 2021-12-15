38d77c25507c43b46028f80b29f5bcc748b139ba
Harshith Gowda <harshithgowdakt@gmail.com>
2021-12-15 23:34:52 +0530

    git rename

---
+++ dev-setup/gitrename.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="old@email.com"
+CORRECT_NAME="Harshith Gowda"
+CORRECT_EMAIL="harshithgowdakt@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
